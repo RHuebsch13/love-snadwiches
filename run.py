d251f9bd7081548fb6ed5f9f3af8495193d8051e
RHuebsch13 <roxannhuebsch@outlook.com>
2024-05-18 10:36:13 +0100

    Add user request loop to repeat request for data until data provided it valid

--- run.py
+++ run.py
@@ -5,35 +5,37 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-]
+    ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-def get_sales_data():
+def get_sales_data() :
     """
     Get sales figures input from the user
     """
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
-        print("Example: 10,20,30,40,50,60\n")
-        
+        print("Example: 10, 20, 30, 40, 50, 60\n")
+    
         data_str = input("Enter your data here: ")
-        
+    
         sales_data = data_str.split(",")
-        
+     
         if validate_data(sales_data):
             print("Data is valid!")
             break
-
-def validate_data(values):
+     
+    
+    
+def validate_data(values): 
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values.
+    or if there aren't exctly 6 values.
     """
     try:
         [int(value) for value in values]
@@ -46,6 +48,6 @@ def validate_data(values):
         return False
     
     return True
-
+    
+    
 get_sales_data()
-
